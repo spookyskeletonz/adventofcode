8372706d8948f0e7192870f63e66e17c9fbada44
Neil.Sarkar <ansarkar@outlook.com>
2019-12-13 17:03:16 +1100

    solved part one

--- src/main/java/Day01/Day01.java
+++ src/main/java/Day01/Day01.java
@@ -1,7 +1,21 @@
 package Day01;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Day01 {
-    public void hello() {
-        System.out.print("hi");
+    public int solveFile(File file) throws FileNotFoundException{
+      Scanner fileScanner = new Scanner(file);
+      int total = 0;
+      while (fileScanner.hasNextInt()) {
+        int mass = fileScanner.nextInt();
+        total += solveInt(mass);
+      }
+
+      return total;
+    }
+
+    private int solveInt(int mass) {
+      return mass/3 - 2;
     }
 }
--- src/test/java/Day01/Day01Test.java
+++ src/test/java/Day01/Day01Test.java
@@ -2,13 +2,31 @@ package Day01;
 
 import org.junit.Test;
 
+import java.io.File;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by arunavsarkar on 13/12/19.
  */
 public class Day01Test {
+  private final Day01 day = new Day01();
+
     @Test
-    public void testHello1() {
-        Day01 day = new Day01();
-        day.hello();
+    public void testOne() throws Exception {
+      File file = new File(getClass().getClassLoader().getResource("Day01/testOne.txt").getFile());
+      assertEquals(2, day.solveFile(file));
+    }
+
+    @Test
+    public void testRoundingAndSum() throws Exception {
+      File file = new File(getClass().getClassLoader().getResource("Day01/testTwo.txt").getFile());
+      assertEquals(656, day.solveFile(file));
+    }
+
+    @Test
+    public void testLargeInput() throws Exception {
+      File file = new File(getClass().getClassLoader().getResource("Day01/input.txt").getFile());
+      assertEquals(3297866, day.solveFile(file));
     }
 }
