cdcea9eef8cb96278651286379fa90c8af106e0f
Neil <ansarkar@outlook.com>
2021-12-05 16:46:54 +1100

    Day 5

--- src/main/java/twentytwentyone/day05/Day05.java
+++ src/main/java/twentytwentyone/day05/Day05.java
@@ -1,4 +1,97 @@
 package twentytwentyone.day05;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Day05 {
+
+  public static class Coord {
+    int x;
+    int y;
+
+    public Coord(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Coord coord = (Coord) o;
+      return x == coord.x && y == coord.y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
+    }
+  }
+
+  public int solvePartOne(File file) throws FileNotFoundException {
+    var scanner = new Scanner(file);
+    var coordMap = loadInputIntoCoordinateMap(scanner, true);
+    var numMultiplePoints = 0;
+    for (var entry : coordMap.entrySet()) {
+      if (entry.getValue() > 1) numMultiplePoints++;
+    }
+    return numMultiplePoints;
+  }
+
+  public int solvePartTwo(File file) throws FileNotFoundException {
+    var scanner = new Scanner(file);
+    var coordMap = loadInputIntoCoordinateMap(scanner, false);
+    var numMultiplePoints = 0;
+    for (var entry : coordMap.entrySet()) {
+      if (entry.getValue() > 1) numMultiplePoints++;
+    }
+    return numMultiplePoints;
+  }
+
+  // Map = Key: Coordinate => Value: Number of lines on that point
+  public Map<Coord, Integer> loadInputIntoCoordinateMap(Scanner fileScanner, boolean onlyHorizVert) {
+    var coordMap = new HashMap<Coord, Integer>();
+    while (fileScanner.hasNextLine()) {
+      var line = fileScanner.nextLine();
+      var coordStrings = line.split(" -> ");
+      var lineStartCoord = new Coord(
+          Integer.parseInt(coordStrings[0].split(",")[0]),
+          Integer.parseInt(coordStrings[0].split(",")[1]));
+      var lineEndCoord = new Coord(
+          Integer.parseInt(coordStrings[1].split(",")[0]),
+          Integer.parseInt(coordStrings[1].split(",")[1]));
+      // Only considering horizontal or vertical lines if flag is set
+      if (onlyHorizVert && lineStartCoord.x != lineEndCoord.x && lineStartCoord.y != lineEndCoord.y) continue;
+
+      var x = lineStartCoord.x;
+      var y = lineStartCoord.y;
+      // Update start of line
+      var key = new Coord(x, y);
+      var numLines = coordMap.getOrDefault(key, 0);
+      numLines++;
+      coordMap.put(key, numLines);
+      // Walk and update coords to end of line
+      while (x != lineEndCoord.x || y != lineEndCoord.y) {
+        if (x < lineEndCoord.x) {
+          x++;
+        } else if (x > lineEndCoord.x) {
+          x--;
+        }
+        if (y < lineEndCoord.y) {
+          y++;
+        } else if (y > lineEndCoord.y) {
+          y--;
+        }
+        key = new Coord(x, y);
+        numLines = coordMap.getOrDefault(key, 0);
+        numLines++;
+        coordMap.put(key, numLines);
+      }
+    }
+    return coordMap;
+  }
 }
--- src/test/java/twentytwentyone/day05/Day05Test.java
+++ src/test/java/twentytwentyone/day05/Day05Test.java
@@ -1,5 +1,35 @@
 package twentytwentyone.day05;
 
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
 public class Day05Test {
   private Day05 day = new Day05();
+
+  @Test
+  public void testPartOneBasic() throws Exception {
+    File file = new File(getClass().getClassLoader().getResource("twentytwentyone/day05/testOne.txt").getFile());
+    assertEquals(5, day.solvePartOne(file));
+  }
+
+  @Test
+  public void testPartOneInput() throws Exception {
+    File file = new File(getClass().getClassLoader().getResource("twentytwentyone/day05/inputOne.txt").getFile());
+    assertEquals(6007, day.solvePartOne(file));
+  }
+
+  @Test
+  public void testPartTwoBasic() throws Exception {
+    File file = new File(getClass().getClassLoader().getResource("twentytwentyone/day05/testOne.txt").getFile());
+    assertEquals(12, day.solvePartTwo(file));
+  }
+
+  @Test
+  public void testPartTwoInput() throws Exception {
+    File file = new File(getClass().getClassLoader().getResource("twentytwentyone/day05/inputOne.txt").getFile());
+    assertEquals(19349, day.solvePartTwo(file));
+  }
 }
