292b245269c7a3beffcbf73043d4271a3ec44139
Neil <ansarkar@outlook.com>
2022-12-13 21:00:44 +1100

    Day 3

--- src/main/java/twentytwentytwo/day03/Day03.java
+++ src/main/java/twentytwentytwo/day03/Day03.java
@@ -1,4 +1,65 @@
 package twentytwentytwo.day03;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Day03 {
+  public int solvePartOne(File file) throws FileNotFoundException {
+    var scanner = new Scanner(file);
+    var prioritySum = 0;
+    while (scanner.hasNextLine()) {
+      var line = scanner.nextLine();
+      var comp1 = line.substring(0, line.length()/2).chars()
+          .mapToObj(i -> (char)i)
+          .collect(Collectors.toSet());
+      var comp2 = line.substring(line.length()/2).chars()
+          .mapToObj(i -> (char)i)
+          .collect(Collectors.toSet());
+      comp1.retainAll(comp2);
+      for (var c : comp1) {
+        if (Character.isUpperCase(c)) {
+          prioritySum += c - 'A' + 27;
+        } else {
+          prioritySum += c - 'a' + 1;
+        }
+      }
+    }
+    return prioritySum;
+  }
+
+  public int solvePartTwo(File file) throws FileNotFoundException {
+    var scanner = new Scanner(file);
+    var prioritySum = 0;
+    var group = new ArrayList<String>();
+    while (scanner.hasNextLine()) {
+      var line = scanner.nextLine();
+      group.add(line);
+      if (group.size() == 3) {
+        var comp1 = group.get(0).chars()
+            .mapToObj(i -> (char)i)
+            .collect(Collectors.toSet());
+        comp1.retainAll(group.get(1).chars()
+            .mapToObj(i -> (char)i)
+            .collect(Collectors.toSet()));
+        comp1.retainAll(group.get(2).chars()
+            .mapToObj(i -> (char)i)
+            .collect(Collectors.toSet()));
+        for (var c : comp1) {
+          if (Character.isUpperCase(c)) {
+            prioritySum += c - 'A' + 27;
+          } else {
+            prioritySum += c - 'a' + 1;
+          }
+        }
+
+        group.clear();
+      }
+    }
+    return prioritySum;
+  }
 }
--- src/test/java/twentytwentytwo/day03/Day03Test.java
+++ src/test/java/twentytwentytwo/day03/Day03Test.java
@@ -1,4 +1,35 @@
 package twentytwentytwo.day03;
 
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
 public class Day03Test {
+  private final Day03 day = new Day03();
+
+  @Test
+  public void testPartOneBasic() throws Exception {
+    var file = new File(getClass().getClassLoader().getResource("twentytwentytwo/day03/testOne.txt").getFile());
+    assertEquals(157, day.solvePartOne(file));
+  }
+
+  @Test
+  public void testPartOneInput() throws Exception {
+    var file = new File(getClass().getClassLoader().getResource("twentytwentytwo/day03/inputOne.txt").getFile());
+    assertEquals(8394, day.solvePartOne(file));
+  }
+
+  @Test
+  public void testPartTwoBasic() throws Exception {
+    var file = new File(getClass().getClassLoader().getResource("twentytwentytwo/day03/testOne.txt").getFile());
+    assertEquals(70, day.solvePartTwo(file));
+  }
+
+  @Test
+  public void testPartTwoInput() throws Exception {
+    var file = new File(getClass().getClassLoader().getResource("twentytwentytwo/day03/inputOne.txt").getFile());
+    assertEquals(2413, day.solvePartTwo(file));
+  }
 }
